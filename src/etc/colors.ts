752fc44cc1ffcdd1736e5dca08a450075e8ee91f
darkobits <joshua@frontlawn.net>
2018-08-10 08:06:47 -0700

    refactor: Colors are defined using HSL.

--- src/etc/colors.ts
+++ src/etc/colors.ts
@@ -1,37 +1,99 @@
 import Color from '../lib/color';
 
 
+const LIGHTEN_COEFFICIENT = 0;
+
+
 // ----- Canonical Colors ------------------------------------------------------
 
-export const green = new Color('#A3BE8C');
-export const red = new Color('#BF616A');
-export const orange = new Color('#D08770');
-export const yellow = new Color('#EBCB8B');
-export const blue = new Color('#8FA1B3');
-export const purple = new Color('#B48EAD');
-export const rust = new Color('#AB7967');
-export const seafoam = new Color('#96B5B4');
+export const green = new Color({
+  h: 92, s: 28, l: 65
+}).lighten(LIGHTEN_COEFFICIENT);
 
-export const gray1 = new Color('#232830');
-export const gray2 = new Color('#343D46');
-export const gray3 = new Color('#4F5B66');
-export const gray4 = new Color('#65737E');
-export const gray5 = new Color('#A7ADBA');
-export const gray6 = new Color('#C0C5CE');
-export const gray7 = new Color('#EFF1F5');
+export const red = new Color({
+  h: 354, s: 42, l: 56
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const orange = new Color({
+  h: 14, s: 51, l: 63
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const yellow = new Color({
+  h: 40, s: 71, l: 73
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const blue = new Color({
+  h: 210, s: 19, l: 63
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const purple = new Color({
+  h: 311, s: 20, l: 63
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const rust = new Color({
+  h: 16, s: 29, l: 54
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const seafoam = new Color({
+  h: 178, s: 17, l: 65
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const gray1 = new Color({
+  h: 217, s: 16, l: 16
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const gray2 = new Color({
+  h: 210, s: 15, l: 24
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const gray3 = new Color({
+  h: 209, s: 13, l: 35
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const gray4 = new Color({
+  h: 206, s: 11, l: 45
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const gray5 = new Color({
+  h: 221, s: 12, l: 69
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const gray6 = new Color({
+  h: 219, s: 13, l: 78
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const gray7 = new Color({
+  h: 220, s: 23, l: 95
+}).lighten(LIGHTEN_COEFFICIENT);
 
 
 // ----- Other Colors ----------------------------------------------------------
 
-export const black = new Color('#000000');
-export const white = new Color('#FFFFFF');
-export const transparent = black.alpha(0);
+export const black = new Color({
+  h: 0, s: 0, l: 0
+}).lighten(LIGHTEN_COEFFICIENT);
 
-export const darkRed = red.desaturate(0.04).darken(0.27);
+export const white = new Color({
+  h: 0, s: 100, l: 100
+}).lighten(LIGHTEN_COEFFICIENT);
 
-export const darkGray1 = new Color('#13151A');
-export const darkGray2 = new Color('#17191F');
-export const darkGray3 = new Color('#1C1F26');
+export const transparent = black.alpha(0).lighten(LIGHTEN_COEFFICIENT);
+
+export const darkRed = red.desaturate(0.04).darken(0.27).lighten(LIGHTEN_COEFFICIENT);
+
+export const darkGray1 = new Color({
+  h: 223, s: 16, l: 9
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const darkGray2 = new Color({
+  h: 225, s: 15, l: 11
+}).lighten(LIGHTEN_COEFFICIENT);
+
+export const darkGray3 = new Color({
+  h: 222, s: 15, l: 13
+}).lighten(LIGHTEN_COEFFICIENT);
 
 // Used for debugging.
-export const HOT_PINK = new Color('#FF00FF');
+export const HOT_PINK = new Color({
+  h: 300, s: 100, l: 50
+});
