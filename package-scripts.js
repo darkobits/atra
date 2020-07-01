5761f45d80312ae3486a31b4b7b24160c4165c4e
darkobits <joshua+github@frontlawn.net>
2020-07-01 00:13:09 -0700

    chore: Update dependencies.

--- package-scripts.js
+++ package-scripts.js
@@ -1,10 +1,10 @@
-const npsUtils = require('nps-utils');
-
-module.exports = require('@darkobits/ts-unified/dist/config/package-scripts')({
+module.exports = require('@darkobits/ts-unified/dist/config/package-scripts')(() => ({
   scripts: {
+    postbuild: 'del themes && vsct compile',
+    prepublishOnly: 'vsct compile',
     start: {
       description: 'Continuously re-build the project and theme file.',
       script: 'nps build.watch & NODE_ENV=development vsct start'
     }
   }
-});
+}));
