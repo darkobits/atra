246e61abdabf05a73b5202ac3af27598bc72bf0e
darkobits <joshua+github@frontlawn.net>
2021-05-20 00:59:03 -0700

    chore: Install dev builds as separate theme.

--- vsct.config.js
+++ vsct.config.js
@@ -10,9 +10,12 @@ module.exports = {
   // Output folder for compiled themes. N.B. This folder should be listed in our
   // package.json "files" array.
   outDir: 'themes',
+  // Directory name to use in the VS Code extensions folder.
+  installDir: `darkobits.astra${useDevTag ? '-dev' : ''}`,
   // Themes provided by this package.
   themes: [{
     label: `${useDevTag ? '[DEV] ' : ''}Astra`,
+    // outputFilename: useDevTag ? 'astra-dev' : undefined,
     path: 'dist/astra.js',
     uiTheme: 'vs-dark'
   }]
